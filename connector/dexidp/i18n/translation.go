130abd3de767b461abbd155ebd471d06ad399cf3
Itamar <itamarkn60@gmail.com>
2023-06-06 21:26:35 +0300

    chore: Creating a plugin directory for dexidp

---
+++ connector/dexidp/i18n/translation.go
@@ -0,0 +1,11 @@
+package i18n
+
+const (
+	ConnectorName                 = "plugin.dexidp_connector.backend.name"
+	InfoName                      = "plugin.dexidp_connector.backend.info.name"
+	InfoDescription               = "plugin.dexidp_connector.backend.info.description"
+	ConfigClientIDTitle           = "plugin.dexidp_connector.backend.config.client_id.title"
+	ConfigClientIDDescription     = "plugin.dexidp_connector.backend.config.client_id.description"
+	ConfigClientSecretTitle       = "plugin.dexidp_connector.backend.config.client_secret.title"
+	ConfigClientSecretDescription = "plugin.dexidp_connector.backend.config.client_secret.description"
+)
